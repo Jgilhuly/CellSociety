9ca196330845b489535f8160f2e1e83db517f224
Jangsoon <jp148@duke.edu>
2015-02-02 01:00:26 -0500

    cell.java and state.java

---
+++ src/cellsociety_team01/Cell.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+Class Cell {
+
+	private int myX;
+	private int myY;
+	private State myCurState;
+	private State myNextState;
+
+	public Cell(int x, int y, Color c) {
+		myX = x;
+		myY = y;
+		myCurState = new State(c);
+		myNextState = null;
+	}
+
+	public void update() {
+		myCurState = myNextState;
+	}	
+
+	public State findNextState(Cell[] cellArray, Rule myRule) {
+		State s = myRule.determineNextState(myCurState, cellArray);
+		myNextState = s;
+	}	
+
+	public void setX(int x) {
+		myX = x;
+	}
+
+	public void setY(int y) {
+		myY = y;
+	}
+
+	public int getX() {
+		return myX;
+	}
+
+	public int getY() {
+		return myY;
+	}
+
+	public State getCurState() {
+		return myCurState;
+	}
+
+	public State getNextState() {
+		return myNextState;
+	}
+
+	public void setCurState(State s) {
+		myCurState = s;
+	}
+
+	public void setNextState(State s) {
+		myNextState = s;
+	}
+
+}
