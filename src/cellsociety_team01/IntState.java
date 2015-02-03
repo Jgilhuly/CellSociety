2f6a9579fe244b68e1969ef80769ace5578306f8
Jangsoon <jp148@duke.edu>
2015-02-03 01:28:22 -0500

    IntState

---
+++ src/cellsociety_team01/IntState.java
@@ -0,0 +1,16 @@
+package cellsociety_team01;
+import javafx.scene.paint.Color;
+
+public class IntState extends State {
+
+	private int stateNum;
+
+	public IntState(int i, Color c, String s){
+		super(c, s);
+		stateNum = i;
+	}
+
+	public int getInt() {
+		return stateNum;
+	}
+}
