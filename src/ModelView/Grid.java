3e9c06a981b004227981b7a5d883698b4502d1cd
Jgilhuly <jrg37@duke.edu>
2015-02-02 20:33:24 -0500

    Added neighbor finding methods

--- src/ModelView/Grid.java
+++ src/ModelView/Grid.java
@@ -1,11 +1,13 @@
 package ModelView;
 
+import java.util.ArrayList;
+
 import javafx.animation.KeyFrame;
 import javafx.util.Duration;
 
 public class Grid {
-	// Rule myRule;
-	// private Cell[][] cells;
+	Simulation simulation;
+	private Cell[][] cells;
 	private GUI myView;
 	private boolean simRunning;
 	private double updateRate;
@@ -40,11 +42,15 @@ public class Grid {
 	public void changeUpdateRate(double newRate) {
 		updateRate = newRate;
 	}
-	
-//	public void updateGrid(Cell[][] cellsIn) {
-//		cells = cellsIn;
-//	}
-	
+
+	public void setSimulation(Simulation simulationIn) {
+		simulation = simulationIn;
+	}
+
+	public void updateGrid(Cell[][] cellsIn) {
+		cells = cellsIn;
+	}
+
 	public void setTitle(String titleIn) {
 		myView.getStage().setTitle(titleIn);
 	}
@@ -62,22 +68,178 @@ public class Grid {
 		myView.update();
 	}
 
-	//	public Cell[][] getCells() {
-	//		return cells;
-	//	}
-
-	private void updateCells() {
-		// for (int i = 0; i < cells.length; i++) {
-		// for (int j = 0; j < cells[i].length; i++) {
-		//		cells[i][j].findNextState();
-		// }
-		// }
-
-		// for (int i = 0; i < cells.length; i++) {
-		// for (int j = 0; j < cells[i].length; i++) {
-		//		cells[i][j].updateState();
-		// }
-		// }
+	public Cell[][] getCells() {
+		return cells;
 	}
 
+	private void updateCells() {
+		Cell[] neighbors;
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = 0; j < cells[i].length; i++) {
+				if (simulation.useDiagonals())
+				{
+					neighbors = find8Neighbors(i, j);
+				}
+				else {
+					neighbors = find4Neighbors(i, j);
+				}
+				cells[i][j].findNextState(neighbors, simulation);
+				cells[i][j].updateState(neighbors, simulation);
+			}
+		}
+	}
+
+	private ArrayList<Cell> find8Neighbors(int row, int col) {
+		ArrayList<Cell> neighbors = new ArrayList<Cell>();
+
+		int rows = cells.length;
+		int cols = cells[0].length;
+
+		if (col < cols-2) {
+			neighbors.add(cells[row][col+1]);
+		}
+		if (col > 0) {
+			neighbors.add(cells[row][col-1]);
+		}
+		if (row > 0) {
+			neighbors[2].add(cells[row-1][col]);
+		}
+		if (row < rows-2) {
+			neighbors.add(cells[row+1][col]);
+		}
+		if (col < cols-2 && row < rows-2) {
+			neighbors.add(cells[row+1][col+1]);
+		}
+		if (row > 0 && col < cols-2) {
+			neighbors.add(cells[row-1][col+1]);
+		}
+		if (row < rows-2 && col > 0) {
+			neighbors.add(cells[row+1][col-1]);
+		}
+		if (row > 0 && col > 0)
+			neighbors.add(cells[row-1][col-1]);
+
+		return neighbors;
+	}
+
+	private ArrayList<Cell> find4Neighbors(int row, int col) {
+		ArrayList<Cell> neighbors = new ArrayList<Cell>();
+
+		int rows = cells.length;
+		int cols = cells[0].length;
+
+		if (col < cols-2) {
+			neighbors.add(cells[row][col+1]);
+		}
+		if (col > 0) {
+			neighbors.add(cells[row][col-1]);
+		}
+		if (row > 0) {
+			neighbors[2].add(cells[row-1][col]);
+		}
+		if (row < rows-2) {
+			neighbors.add(cells[row+1][col]);
+		}
+		return neighbors;
+	}
+
+	private ArrayList<Cell> find8NeighborsWrap(int row, int col) {
+		ArrayList<Cell> neighbors = new ArrayList<Cell>();
+
+		int rows = cells.length;
+		int cols = cells[0].length;
+
+		if (col < cols-2) {
+			neighbors.add(cells[row][col+1]);
+		}
+		else {
+			neighbors.add(cells[row][0]);
+		}
+		
+		if (col > 0) {
+			neighbors.add(cells[row][col-1]);
+		}
+		else {
+			neighbors.add(cells[row][cols]);
+		}
+		
+		if (row > 0) {
+			neighbors[2].add(cells[row-1][col]);
+		}
+		else {
+			neighbors[2].add(cells[rows][col]);
+		}
+		
+		if (row < rows-2) {
+			neighbors.add(cells[row+1][col]);
+		}
+		else {
+			neighbors.add(cells[0][col]);
+		}
+		
+		if (col < cols-2 && row < rows-2) {
+			neighbors.add(cells[row+1][col+1]);
+		}
+		else {
+			neighbors.add(cells[0][0]);
+		}
+		
+		if (row > 0 && col < cols-2) {
+			neighbors.add(cells[row-1][col+1]);
+		}
+		else {
+			neighbors.add(cells[rows][0]);
+		}
+		
+		if (row < rows-2 && col > 0) {
+			neighbors.add(cells[row+1][col-1]);
+		}
+		else {
+			neighbors.add(cells[0][cols]);
+		}
+		
+		if (row > 0 && col > 0) {
+			neighbors.add(cells[row-1][col-1]);
+		}
+		else {
+			neighbors.add(cells[rows][cols]);
+		}
+
+		return neighbors;
+	}
+	
+	private ArrayList<Cell> find4NeighborsWrap(int row, int col) {
+		ArrayList<Cell> neighbors = new ArrayList<Cell>();
+
+		int rows = cells.length;
+		int cols = cells[0].length;
+
+		if (col < cols-2) {
+			neighbors.add(cells[row][col+1]);
+		}
+		else {
+			neighbors.add(cells[row][0]);
+		}
+		
+		if (col > 0) {
+			neighbors.add(cells[row][col-1]);
+		}
+		else {
+			neighbors.add(cells[row][cols]);
+		}
+		
+		if (row > 0) {
+			neighbors[2].add(cells[row-1][col]);
+		}
+		else {
+			neighbors[2].add(cells[rows][col]);
+		}
+		
+		if (row < rows-2) {
+			neighbors.add(cells[row+1][col]);
+		}
+		else {
+			neighbors.add(cells[0][col]);
+		}
+	}
 }
