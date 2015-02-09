5bc81486ad88fc4ca1825c8d54452f3818c1907c
Jgilhuly <jrg37@duke.edu>
2015-02-08 20:36:02 -0500

    Reorganized GUI Code

--- src/cellsociety_team01/modelview/GUI.java
+++ src/cellsociety_team01/modelview/GUI.java
@@ -80,6 +80,115 @@ public class GUI {
 
 		myScene = new Scene(root, myWidth, myHeight);
 	}
+	
+	/**
+	 * Makes the Buttons and Slider
+	 * @return
+	 */
+	private Node makeButtonsAndSlider() {
+		HBox result = new HBox();
+		playButton = makeButton("PlayCommand", event -> myModel.play());
+		result.getChildren().add(playButton);
+		pauseButton = makeButton("PauseCommand", event -> myModel.pause());
+		result.getChildren().add(pauseButton);
+		stepButton = makeButton("StepCommand", event -> myModel.step());
+		result.getChildren().add(stepButton);
+		resetButton = makeButton("ResetCommand", event -> reset());
+		result.getChildren().add(resetButton);
+		
+		result.getChildren().add(makeSlider());
+		result.setSpacing(10);
+		
+		return result;
+	}
+	
+	/**
+	 * Helper method to create buttons with labels and handlers (Taken from the example_browser)
+	 * @param property
+	 * @param handler
+	 * @return
+	 */
+	private Button makeButton(String property, EventHandler<ActionEvent> handler) {
+		Button result = new Button();
+		String label = myResources.getString(property);
+		result.setText(label);
+		result.setOnAction(handler);
+		return result;
+	}
+	
+	/**
+	 * Makes the Slider which controls the speed of the simulation
+	 * @return
+	 */
+	private Node makeSlider() {
+		slider = new Slider();
+		slider.setMin(0);
+		slider.setMax(1000);
+		slider.setValue(500);
+		slider.setShowTickLabels(true);
+		slider.setShowTickMarks(true);
+		slider.setMajorTickUnit(500);
+		slider.setMinorTickCount(10);
+		return slider;
+	}
+	
+	/**
+	 * Configures the top menu bar
+	 * @return
+	 */
+	private Node makeMenuBar() {
+		Menu menu1 = new Menu(myResources.getString("File"));
+		Menu menu2 = new Menu(myResources.getString("About"));
+
+		MenuItem loadXML = new MenuItem(myResources.getString("LoadXML"));
+		loadXML.setOnAction(e -> loadXML());
+		menu1.getItems().add(loadXML);
+
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(menu1, menu2);
+
+		return menuBar;
+	}
+	
+	/**
+	 * Configures the grid view
+	 * @return
+	 */
+	private Node makeGrid(int sceneWidth, int sceneHeight, int rows, int cols) {
+		gridView = new SquareGridView(sceneWidth, sceneHeight, rows, cols);
+		gridCanvas = gridView.makeGrid(new Canvas(sceneWidth / 2,
+				sceneHeight / 2));
+		gridCanvas.addEventHandler(MouseEvent.MOUSE_CLICKED,
+				e -> cellClicked(e));
+		return gridCanvas;
+	}
+	
+	/**
+	 * Sets up the graph
+	 * @return
+	 */
+	private Node makeGraph() {
+		VBox result = new VBox();
+		
+        NumberAxis xAxis = new NumberAxis();
+        NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("X Axis");
+        yAxis.setLabel("Y Axis");
+        
+        LineChart<Number,Number> lineGraph = 
+                new LineChart<Number,Number>(xAxis,yAxis);
+                
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Name");
+        series.getData().add(new XYChart.Data(1, 23));
+        
+        lineGraph.setTitle("Title");
+        lineGraph.getData().add(series);
+        
+        result.getChildren().add(lineGraph);
+        
+		return result;
+	}
 
 	/**
 	 * Sets up the preferences panel for changing parameters
@@ -112,127 +221,7 @@ public class GUI {
 	}
 
 	/**
-	 * Sets up the graph
-	 * @return
-	 */
-	private Node makeGraph() {
-		VBox result = new VBox();
-		
-        NumberAxis xAxis = new NumberAxis();
-        NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("X Axis");
-        yAxis.setLabel("Y Axis");
-        
-        LineChart<Number,Number> lineGraph = 
-                new LineChart<Number,Number>(xAxis,yAxis);
-                
-        XYChart.Series series = new XYChart.Series();
-        series.setName("Name");
-        series.getData().add(new XYChart.Data(1, 23));
-        
-        lineGraph.setTitle("Title");
-        lineGraph.getData().add(series);
-        
-        result.getChildren().add(lineGraph);
-        
-		return result;
-	}
-
-	/**
-	 * Makes the Buttons and Slider
-	 * @return
-	 */
-	private Node makeButtonsAndSlider() {
-		HBox result = new HBox();
-		playButton = makeButton("PlayCommand", event -> myModel.play());
-		result.getChildren().add(playButton);
-		pauseButton = makeButton("PauseCommand", event -> myModel.pause());
-		result.getChildren().add(pauseButton);
-		stepButton = makeButton("StepCommand", event -> myModel.step());
-		result.getChildren().add(stepButton);
-		resetButton = makeButton("ResetCommand", event -> reset());
-		result.getChildren().add(resetButton);
-
-		slider = new Slider();
-		slider.setMin(0);
-		slider.setMax(1000);
-		slider.setValue(500);
-		slider.setShowTickLabels(true);
-		slider.setShowTickMarks(true);
-		slider.setMajorTickUnit(500);
-		slider.setMinorTickCount(10);
-
-		result.getChildren().add(slider);
-
-		result.setSpacing(10);
-
-		return result;
-	}
-
-	/**
-	 * Resets the simulation to the last xml file
-	 */
-	public void reset() {
-		parser = new Parser(file, myModel);
-		parser.parseXmlFile();
-		setGridCellStates();
-	}
-
-	/**
-	 * Gets stage
-	 * @return
-	 */
-	public Stage getStage() {
-		return myStage;
-	}
-
-	/**
-	 * Helper method to create buttons with labels and handlers (Taken from the example_browser)
-	 * @param property
-	 * @param handler
-	 * @return
-	 */
-	private Button makeButton(String property, EventHandler<ActionEvent> handler) {
-		Button result = new Button();
-		String label = myResources.getString(property);
-		result.setText(label);
-		result.setOnAction(handler);
-		return result;
-	}
-
-	/**
-	 * Configures the top menu bar
-	 * @return
-	 */
-	private Node makeMenuBar() {
-		Menu menu1 = new Menu(myResources.getString("File"));
-		Menu menu2 = new Menu(myResources.getString("About"));
-
-		MenuItem loadXML = new MenuItem(myResources.getString("LoadXML"));
-		loadXML.setOnAction(e -> loadXML());
-		menu1.getItems().add(loadXML);
-
-		MenuBar menuBar = new MenuBar();
-		menuBar.getMenus().addAll(menu1, menu2);
-
-		return menuBar;
-	}
-
-	/**
-	 * Configures the grid view
-	 * @return
-	 */
-	private Node makeGrid(int sceneWidth, int sceneHeight, int rows, int cols) {
-		gridView = new SquareGridView(sceneWidth, sceneHeight, rows, cols);
-		gridCanvas = gridView.makeGrid(new Canvas(sceneWidth / 2,
-				sceneHeight / 2));
-		gridCanvas.addEventHandler(MouseEvent.MOUSE_CLICKED,
-				e -> cellClicked(e));
-		return gridCanvas;
-	}
-
-	/**
-	 * Brings up a file chooser to select an xml file
+	 * Brings up a file chooser to select an XML file
 	 */
 	private void loadXML() {
 		FileChooser fileChooser = new FileChooser();
@@ -249,6 +238,17 @@ public class GUI {
 		}
 
 		setGridCellStates();
+	}
+	
+	/**
+	 * updates the GUI, called by grid on update tick
+	 */
+	public void update(boolean step) {
+		if (myModel.isSimRunning() || step) {
+			setGridCellStates();
+		}
+		myModel.changeUpdateRate(slider.getValue()); // Change This
+		enableButtons();
 	}
 
 	/**
@@ -271,6 +271,15 @@ public class GUI {
 		int cellY = (int) (y / (gridCanvas.getHeight() / numRows));
 		System.out.println(cellX + ", " + cellY);
 	}
+	
+	/**
+	 * Resets the simulation to the last xml file
+	 */
+	public void reset() {
+		parser = new Parser(file, myModel);
+		parser.parseXmlFile();
+		setGridCellStates();
+	}
 
 	/**
 	 * enables / disables buttons on update
@@ -283,20 +292,17 @@ public class GUI {
 	}
 
 	/**
-	 * updates the GUI, called by grid on update tick
-	 */
-	public void update(boolean step) {
-		if (myModel.isSimRunning() || step) {
-			setGridCellStates();
-		}
-		myModel.changeUpdateRate(slider.getValue()); // Change This
-		enableButtons();
-	}
-
-	/**
 	 * Returns scene for this view so it can be added to stage.
 	 */
 	public Scene getScene() {
 		return myScene;
+	}
+	
+	/**
+	 * Gets stage
+	 * @return
+	 */
+	public Stage getStage() {
+		return myStage;
 	}
 }
